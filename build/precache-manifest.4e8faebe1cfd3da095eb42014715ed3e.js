58fa0d92650e2873069dd8e4080b312e12a74d27
Navimaurya <mauryanavis@gmail.com>
2021-11-25 15:17:47 +0530

    init

---
+++ build/precache-manifest.4e8faebe1cfd3da095eb42014715ed3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8001253fbbdc444626f57c92eaadde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e35900c51d51d20ba9c7",
+    "url": "/static/css/main.0b97ad18.chunk.css"
+  },
+  {
+    "revision": "ca1038c1f6495d48c696",
+    "url": "/static/js/2.c236d9d2.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.c236d9d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35900c51d51d20ba9c7",
+    "url": "/static/js/main.7cd41db3.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
